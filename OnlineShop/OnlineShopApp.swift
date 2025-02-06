2a470184bd5a2af37636434eac07032000f304da
Alena <alenabelovadmvn@gmail.com>
2025-02-06 13:15:52 -0500

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Alena Belova  on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
